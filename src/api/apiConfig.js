4871985ebb41d5466834a396c7e687478e14706b
Mikael Ngigi <michael.ngigi@strathmore.edu>
2022-03-13 10:35:55 +0300

    Security issue solved.

--- src/api/apiConfig.js
+++ src/api/apiConfig.js
@@ -1,6 +1,6 @@
 const apiConfig = {
     baseURL: "https://api.themoviedb.org/3/",
-    apiKey: '9d2e6b561bae7d3cc182e78ba8d4dc2a',
+    apiKey: 'Get from TMDB website',
     originalImage: (imgPath) => `https://image.tmdb.org/t/p/original/${imgPath}`,
     w500Image: (imgPath) => `https://image.tmdb.org/t/p/w500/${imgPath}`,
 }
